09543933b54370cccf93b31d601e9d5148b92cea
Yongki-pratama <gith67594@gmail.com>
2023-02-26 17:21:48 +0700

    upload file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
+  const search = (q) => {
+    console.log({ q });
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>DEA MOVIE MANIA</h1>
+        <input placeholder="Cari filem kesayangan..." className="Movie-search" onChange={({ target }) => search(target.value)} />
+        <div className="Movie-container">
+          <div className="Movie-wrapper">
+            <div className="Movie-title">CONTOH PERTAMA</div>
+            <img className="Movie-img" src="" />
+            <div className="Movie-date">26-02-2023</div>
+            <div className="Movie-rate">8,9</div>
+          </div>
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
